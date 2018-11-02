7e5ee9fddfefc8196f024b4999f510a53cc1b7c4
Alessandro Catalano <alessandrocatalano999@gmail.com>
2018-11-02 19:25:40 +0100

    fixato e commentato il codice di esami.py

--- esami.py
+++ esami.py
@@ -5,51 +5,46 @@ import requests
 import json
 from time import localtime, strftime
 
-
-def elemento(riga, sessione, anno):
-	item = {"insegnamento" : "", "docenti" : "", "prima" : ["", "", ""], "seconda" : ["", ""], "terza" : ["", ""], "straordinaria" : ["", ""], "anno" : anno}
-	item["insegnamento"] = (riga[1]).text
-	item["docenti"] = (riga[2]).text
-	for i in range(len(riga))[3:]:
-		if (riga[i]).has_attr("class"):
-			ses_temp = "straordinaria"
-			(item[ses_temp])[i-3-2] = ((riga[i]).text)
-		elif (riga[i]).text.strip() != "":
-			(item[sessione])[i-3] = ((riga[i]).text)
-	return item
-	
 def main():
 	url_esami = ["http://web.dmi.unict.it/corsi/l-31/esami?sessione=1&aa=118", "http://web.dmi.unict.it/corsi/l-31/esami?sessione=2&aa=118", "http://web.dmi.unict.it/corsi/l-31/esami?sessione=3&aa=118"]
 	status = {"length": "" , "lastupdate": strftime("%Y-%d-%m %H:%M:%S", localtime())}
-	arr = ["prima", "seconda", "terza"]
-	items = []
-	anno = ""
-	for count, url in enumerate(url_esami):
-		sorgente = requests.get(url).text
-		soup = bs4.BeautifulSoup(sorgente, "html.parser")
-		table = soup.find(id="tbl_small_font")
-		all_tr = table.find_all("tr")
-		for tr in all_tr:
-			if not tr.has_attr("class"):
-				firstd = tr.find("td")
-				if not firstd.has_attr("class"):
-					all_td = tr.find_all("td")
-					sessione = arr[count]
-					if count == 0:
-						items.append(elemento(all_td, sessione, anno))
-					else:
-						flag = False
-						for element in items:
-							if (all_td[1]).text == element["insegnamento"]:
-								flag = True
-								for i in range(len(all_td))[3:]:
-									(element[sessione])[i-3] = ((all_td[i]).text)
-						if not flag:
-							items.append(elemento(all_td, sessione, anno))					
-				else:
-					anno = firstd.b.text
-	status["length"] = len(items)
-	finaljson = {"status" : status, "items" : items}
+	arraySessione = ["prima", "seconda", "terza"]
+	materie = [] #array che avrà come elementi le materie
+	anno = "" #stringa che contiene l'anno delle materie che stiamo guardando attualmente
+	for count, url in enumerate(url_esami): #count serve a capire quale degli url (e quindi sessione) stiamo scorrendo, esempio se count = 1 siamo alla sessione 2
+		sorgente = requests.get(url).text #prendiamo il file html puro
+		soup = bs4.BeautifulSoup(sorgente, "html.parser") #e lo diamo in pasto a bs4
+		table = soup.find(id="tbl_small_font") #prendiamo direttamente la tabella dato che sappiamo l'id
+		righe = table.find_all("tr") #e dalla tabella estraiamo l'array con tutte le righe
+		for riga in righe: #e scorriamo riga per riga
+			if not riga.has_attr("class"): #se non ha l'attributo class potrebbe essere una materia oppure l'anno (altrimenti è la riga delle informazioni che non ci interessa)
+				primacella = riga.find("td") #estraiamo la prima cella
+				if not primacella.has_attr("class"): #se questa non ha l'attributo class è una materia
+					celle = riga.find_all("td") #adesso che sappiamo che è una materia, estraiamo tutte le celle per ottenere i dati su di essa
+					sessione = arraySessione[count] #in base al valore di count sappiamo la sessione che stiamo analizzando
+					flag = False #variabile sentinella per vedere se la materia che stiamo analizzando è già presente dentro l'array
+					for materia in materie: #scorriamo tutte le materie fino ad ora inserite (inizialmente, banalmente, saranno 0)
+						if (celle[1]).text == materia["insegnamento"]: #se abbiamo trovato la materia nell'array
+							flag = True #setto la sentinella a true che indica che la materia era già presente nell'array delle materia dunque dobbiamo solo aggiungere gli appelli della nuova sessione>1
+							for cella in celle[3:]: #dato che la materia è già presente nell'array, i primi 3 valori (id, docenti e nome) non ci interessano
+								if(cella.has_attr("class")): #se la cella ha l'attributo class allora è un'appello straordinario
+									(materia["straordinaria"]).append(cella.text)
+								elif(cella.text.strip() != ""): #altrimenti è un appello della sessione che stiamo analizzando
+									(materia[sessione]).append(cella.text)
+					if not flag: #se non abbiamo trovato la materia che stiamo analizzando attualmente nell'array delle materie vuol dire che nelle sessioni precedenti non aveva appelli (oppure è la prima sessione)
+						nuovaMateria = {"insegnamento" : "", "docenti" : "", "prima" : [], "seconda" : [], "terza" : [], "straordinaria" : [], "anno" : anno} #creiamo l'oggetto della nuova materia
+						nuovaMateria["insegnamento"] = celle[1].text
+						nuovaMateria["docenti"] = celle[2].text
+						for cella in celle[3:]: #come sopra (riga ~29)
+							if(cella.has_attr("class")):
+								(nuovaMateria["straordinaria"]).append(cella.text)
+							elif(cella.text.strip() != ""):
+								(nuovaMateria[sessione]).append(cella.text)
+						materie.append(nuovaMateria) #infine, aggiungiamo la nuova materia all'array delle materie
+				else: #altrimenti, se ha l'attributo class, è la riga che indica l'anno delle materie successive
+					anno = primacella.b.text #quindi aggiorniamo la variabile anno con il valore della prima cella della riga
+	status["length"] = len(materie)
+	finaljson = {"status" : status, "items" : materie}
 	with open('esami.json', 'w') as outfile:
 		json.dump(finaljson, outfile, sort_keys=True, indent=4)
 
